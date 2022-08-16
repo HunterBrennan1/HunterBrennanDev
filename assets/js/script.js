f831cf6252ebdca89bdaa68ffdb3636035f4986f
Hunter <hunterbrennan15@gmail.com>
2022-08-16 13:39:22 -0400

    fixed missing ; in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,6 @@ hamburger.addEventListener("click", () => {
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active")
-  navMenu.classList.remove("active")
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }))
